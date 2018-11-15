952381d14b8d3de651193e6d671245c2d1888ca1
Brannium <JoHab211@web.de>
2018-11-15 21:12:55 +0100

    v0.4.2

--- autoclear_manager.py
+++ autoclear_manager.py
@@ -9,8 +9,10 @@ async def ex(client, before, after):
     config = ConfigManager.getConfig(after.server)["autoclear"]
 
     if config["enabled"]:
+        print('enabled')
         for channel_id, user_id in config["links"].items():
             if user_id == after.id:
+                print('userid')
                 if after.voice.voice_channel is None and before.voice.voice_channel is not None:
                     channel = discord.utils.get(after.server.channels, id=channel_id)
                     print('Clearing channel %s as %s left the voice channel' % (channel.name, user_id))
